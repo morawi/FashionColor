bdf95ab73c8ed8b6b8bc9b8623c54d412d69f5a8
Al-Rawi <ms.alrawi@gmail.com>
2020-06-26 10:58:34 +0100

    Added cluster 3d-1d

--- color_clustering.py
+++ color_clustering.py
@@ -8,83 +8,80 @@ Color Clustering
 
 """
 
-from clothcoparse_dataset import ImageDataset 
-from modanet_dataset import ModanetDataset 
 import argparse
 import platform
 from color_extractor import ColorExtractor as color_extractor_obj
+from color_utils import get_dataset
+from color_table import ColorTable
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--epoch", type=int, default=0, help="epoch to start training from")
-parser.add_argument("--num_epochs", type=int, default=300, help="number of epochs of training")
+
+parser.add_argument("--num_colors", type=int, default=32, help="number of epochs of training")
+parser.add_argument("--max_num_colors", type=int, default=2, help="number of epochs of training")
 parser.add_argument("--dataset_name", type=str, default="ClothCoParse", help="name of the dataset: ClothCoParse or Modanet ")
-parser.add_argument("--batch_size", type=int, default=2, help="size of the batches")
-parser.add_argument("--lr", type=float, default=0.005, help="learning rate")
-parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
-parser.add_argument("--evaluate_interval", type=int, default=50, help="interval between sampling of images from generators")
-parser.add_argument("--checkpoint_interval", type=int, default=50, help="interval between model checkpoints")
-parser.add_argument("--train_percentage", type=float, default=0.9, help="percentage of samples used in training, the rest used for testing")
-parser.add_argument("--experiment_name", type=str, default=None, help="name of the folder inside saved_models")
-parser.add_argument("--print_freq", type=int, default=100, help="progress print out freq")
-parser.add_argument("--lr_scheduler", type=str, default='OneCycleLR', help="lr scheduler name, one of: OneCycleLR, CyclicLR StepLR, ExponentialLR ")
-parser.add_argument("--job_name", type=str, default='test', help=" name for the job used in slurm ")
-
 parser.add_argument("--HPC_run", default=False, type=lambda x: (str(x).lower() == 'true'), help="True/False; -default False; set to True if running on HPC")
-parser.add_argument("--remove_background", default=False, type=lambda x: (str(x).lower() == 'true'), help="True/False; - default False; set to True to remove background from image ")
-parser.add_argument("--person_detection", default=False, type=lambda x: (str(x).lower() == 'true'), help=" True/False; - default is False;  if True will build a model to detect persons")
-parser.add_argument("--train_shuffle", default=True, type=lambda x: (str(x).lower() == 'true'), help="True/False; -default True to shuffle training samples")
-parser.add_argument("--redirect_std_to_file", default=False, type=lambda x: (str(x).lower() == 'true'),  help="True/False - default False; if True sets all console output to file")
-parser.add_argument('--pretrained_model', default=True, type=lambda x: (str(x).lower() == 'true'), help="True/False: default True; True uses a pretrained model")
+parser.add_argument('--save_fig_as_images', default=True, type=lambda x: (str(x).lower() == 'true'), help="True/False: default True; True uses a pretrained model")
+# parser.add_argument("--lr", type=float, default=0.005, help="learning rate")
 
-opt = parser.parse_args()
+
+cnf = parser.parse_args()
 
 if platform.system()=='Windows':
-    opt.n_cpu= 0
+    cnf.n_cpu= 0
 
 
 ''' # number of colors should be equal or more than three,  to count for background, 
 if 2 is used, backgound will be mixed with the original color and will cause problems 
 and incorrect results '''
-num_colors = 64
-max_num_colors = 2
-save_fig_as_images = True
 
-if max_num_colors > num_colors:
+if cnf.max_num_colors > cnf.num_colors:
     print('max_num_colors should be less than or equal than number_of_colors')
     exit()
 
 
-def get_dataset(opt):    
-    if opt.dataset_name=='ClothCoParse':
-        dataset = ImageDataset("../data/%s" % opt.dataset_name, 
-                                transforms_ = None, 
-                                transforms_target= None,
-                                mode="train",                          
-                                HPC_run=opt.HPC_run, 
-                                remove_background = opt.remove_background,   
-                                person_detection = opt.person_detection
-                            )
-    else:
-        dataset = ModanetDataset("../data/%s" % opt.dataset_name, 
-                                 transforms_ = None,                             
-                                 HPC_run= opt.HPC_run, )
-    return dataset
 
-def generate_all_colors(dataset, number_of_colors, max_num_colors):    
+
+def generate_all_colors(dataset, cnf):    
     all_persons_items = []    
-    ids  =range(2) # range(len(dataset))
-    ids = [975]
+    # ids  = range(len(dataset))
+    ids = [271]
+    ids = [58]
+    ids =[192]
     for i in ids:    
         image, masked_img, labels, image_id, masks, im_name = dataset[i]   
         one_person_clothing_colors = color_extractor_obj(image, masks, labels, masked_img, 
-                                                         number_of_colors = num_colors, 
-                                                         max_num_colors = max_num_colors)  
+                                                         number_of_colors = cnf.num_colors, 
+                                                         max_num_colors = cnf.max_num_colors,
+                                                         use_quantize=cnf.use_quantize)  
         
-        fname = im_name if save_fig_as_images else None
-        one_person_clothing_colors.pie_cluster(image, fname=fname, figure_size=(4, 4))
+        fname = im_name if cnf.save_fig_as_images else None
+        one_person_clothing_colors.pie_chart(image, fname=fname, figure_size=(4, 4))
         all_persons_items.append(one_person_clothing_colors)
     return all_persons_items, image
+
+
+def fill_color_table(dataset, cnf):        
+    ids = [271]
+    ids = [58]
+    color_table_obj = ColorTable()    
+    for i in ids:    
+        image, masked_img, labels, image_id, masks, im_name = dataset[i]   
+        one_person_clothing_colors = color_extractor_obj(image, masks, labels, masked_img, 
+                                                         number_of_colors = cnf.num_colors, 
+                                                         max_num_colors = cnf.max_num_colors,
+                                                         use_quantize=cnf.use_quantize)  
+        color_table_obj.add_fashion_item(one_person_clothing_colors)
+    return 
     
-dataset = get_dataset(opt)
-x, image = generate_all_colors(dataset, num_colors, max_num_colors)
+
+
+cnf.num_colors = 13
+cnf.max_num_colors= 4
+cnf.use_quantize = True
+cnf.method = '3d_1d' #{'3d_1d', '3d'}
+dataset = get_dataset(cnf)
+x, image = generate_all_colors(dataset, cnf)
+x[0][0]['skin']
+
+
--- color_extractor.py
+++ color_extractor.py
@@ -8,83 +8,213 @@ Created on Mon Jun 15 16:44:59 2020
 import numpy as np
 from sklearn.cluster import KMeans
 from collections import Counter
-import matplotlib
-# matplotlib.use("TkAgg")
-# backend string are ['GTK3Agg', 'GTK3Cairo', 'MacOSX', 'nbAgg', 'Qt4Agg', 
-# 'Qt4Cairo', 'Qt5Agg', 'Qt5Cairo', 'TkAgg', 'TkCairo', 'WebAgg', 'WX', 'WXAgg',
-# 'WXCairo', 'agg', 'cairo', 'pdf', 'pgf', 'ps', 'svg', 'template']
 import matplotlib.pyplot as plt
+from PIL import Image
+from color_utils import merge_clusters, RGB2HEX
+ 
 
 
-def RGB2HEX(rgb):
-    return "#{:02x}{:02x}{:02x}".format(int(rgb[0]), int(rgb[1]), int(rgb[2]))
+
 
 class ColorExtractor():
     ''' Extracts the colors of one person fashion items/clothing'''
-    def __init__(self, image, masks, labels, masked_img, number_of_colors = 16, max_num_colors = 1):
-        ''' Extracts the colors of one person fashion items/clothing'''
+    def __init__(self, image, masks, labels, masked_img, cnf):
+                                                       
         # inputed
         self.labels = labels     
-        self.number_of_colors = number_of_colors
-        self.max_num_colors = max_num_colors
-        # computed
-        self.counts_top_k=[]
-        self.colors_centers = []
-        self.find_colors(image, masks, masked_img, number_of_colors, max_num_colors)
+        self.number_of_colors = cnf.num_colors
+        self.max_num_colors = cnf.max_num_colors
         
-    def pie_cluster(self, image, figure_size=(9, 6), fname=None):
-        for j in range(len(self.labels)):
-            hex_colors = [RGB2HEX(self.colors_centers[j][i]) for i in self.counts_top_k[j].keys()]    
+        # computed
+        self.top_k_colors=[]
+        self.colors_centers = []                     
+                
+        if cnf.method == '3D_1D':
+            self.find_colors_via_clustering_3D_1D_cntPix(masks, masked_img, 
+                                                         cnf.number_of_colors, 
+                                                         cnf.max_num_colors)
+        elif cnf.method == '3D':              
+            self.find_colors_via_clustering_3D_1D(masks, masked_img, cnf.number_of_colors, 
+                                            cnf.max_num_colors, use_quantize=cnf.use_quantize)
+       
+
+
+    def pie_chart(self, image, figure_size=(9, 6), fname=None):
+        
+        for j in range(len(self.labels)):            
+            hex_colors = [RGB2HEX(self.top_k_colors[j][key]) for key in self.top_k_colors[j].keys()]    
             plt.figure(figsize = figure_size)
             plt.suptitle( self.labels[j] , fontsize=16)
-            plt.pie(self.counts_top_k[j].values(), labels = hex_colors, colors = hex_colors,
-                    rotatelabels = False)
-            plt.show()
+            num_pixel_percent =[k*100 for k in self.top_k_colors[j].keys()]            
+            plt.pie(num_pixel_percent, labels = hex_colors, colors = hex_colors,
+                    rotatelabels = False)            
+                       
             if fname:
-                plt.savefig('./Figures/' + fname + '_ncol='+ str(self.number_of_colors) + '_' + self.labels[j] +'.png')
-                image.save('./Figures/' + fname +'.jpg')
-
+                plt.savefig('./Figures/' + fname + '_ncol='+ str(self.number_of_colors) + '_' + self.labels[j] +'.png')                
+            plt.show()
+            plt.close()
+        if fname: image.save('./Figures/' + fname +'.jpg') # saving the original image for comparision
+        plt.show()
+        plt.close()
         
-    def remove_image_background(self, image, mask):
-        mask =  np.concatenate(mask)
-        modified_image = image.reshape(image.shape[0]*image.shape[1], 3)  
-        modified_image = modified_image[mask] # removing the background
-        return modified_image
     
+    
+    def remove_image_background(self, image, mask):
+        mask =  np.concatenate(mask)  # inpus mask is 2D array, output mask is a vector from concatnating the input 2D array
+        image_no_bkg = image.reshape(image.shape[0]*image.shape[1], 3)  # reshape to a vector with triplet values as entry, each row has R, G, B values
+        image_no_bkg = image_no_bkg[mask] # removing the background via the mask
+        return image_no_bkg
+    
+        
     def get_colors_cluster(self, image, number_of_colors):      
         clf = KMeans(n_clusters = self.number_of_colors, n_jobs=10,
                       max_iter=3000, n_init=16, init='k-means++') # we are using higher number of max_iter and n_init to ensure convergence
         pred_labels = clf.fit_predict(image)
         counts = Counter(pred_labels)    
-        colors_centers = clf.cluster_centers_.round()        
+        colors_centers = np.uint8(clf.cluster_centers_.round())
         return counts, colors_centers
         
-    def get_top_k_colors_sorted(self, counts, k=4):      
-        cnt = counts.most_common() # counts is casted as a sorted dictionary here into cnt   
-        ''' May be we select the top k as a list, we don't need the counter anymore' '''
-          
-        counts_top_k = Counter() # creating a new counter to store the top k colors
-        for i in range(k):        
-            key = cnt[i][0] # key , the negative to start from the end of the list, which has the highest value, list is sorted
-            num_pixels = cnt[i][1] # value
-            counts_top_k[key] = num_pixels
+
+    def find_colors_via_clustering_3D(self, masks, masked_img, number_of_colors, max_num_colors, use_quantize=False):
+        ''' Clustering 3D is based on three-values; R, G, B
+        use_num_pixels_percentage (p) the percentage denotes the probability of a color, 
+        since we are picking max_num_colors out of the available 
+        number_of_colors the sum will not be 1, the sum will one
+        if and only if max_num_colors equals number_of_colors 
+        '''
+        use_num_pixels_percentage = True 
+        for label_id in range(len(self.labels)):   
+            num_pixels_in_mask = np.sum(masks[label_id]) if use_num_pixels_percentage else 1
+            image  = masked_img[label_id]
+            if use_quantize:
+                image = Image.fromarray(masked_img[label_id])
+                image = image.quantize(colors=number_of_colors, method=None, kmeans=0, palette=None).convert('RGB')
+                image = np.array(image)               
             
-        return counts_top_k
-            
-    def find_colors(self, image, masks, masked_img, number_of_colors, max_num_colors):
-        for j in range(len(self.labels)):
-            image_no_bkg = self.remove_image_background(masked_img[j], mask = masks[j])
+            image_no_bkg = self.remove_image_background(image, mask = masks[label_id])
             counts, colors_centers = self.get_colors_cluster(image_no_bkg, number_of_colors=number_of_colors)  
-            counts_top_k = self.get_top_k_colors_sorted(counts, k = max_num_colors)  
-            self.counts_top_k.append(counts_top_k)
-            self.colors_centers.append(colors_centers)
+            top_k_colors = counts.most_common()[:max_num_colors] 
+            x= dict(top_k_colors)            
+            numpixels_and_colors = []       
+            for key in x.keys(): numpixels_and_colors.append( [x[key]/num_pixels_in_mask, colors_centers[key]])                                   
+            self.top_k_colors.append(dict(numpixels_and_colors))
+    
+    def find_colors_via_clustering_3D_1D(self, masks, masked_img, number_of_colors, max_num_colors):
+        ''' Clustering_3D_1D is based on three-values; R, G, B, and then, on 1D via h value from hsv
+        use_num_pixels_percentage (p) the percentage denotes the probability of a color, 
+        since we are picking max_num_colors out of the available 
+        number_of_colors the sum will not be 1, the sum will one
+        if and only if max_num_colors equals number_of_colors 
+        '''
+        use_num_pixels_percentage = True 
+        for label_id in range(len(self.labels)):   
+            num_pixels_in_mask = np.sum(masks[label_id]) if use_num_pixels_percentage else 1
+            image  = masked_img[label_id]           
+            image_no_bkg = self.remove_image_background(image, mask = masks[label_id])
+            
+            # use clustering to reduce the number of colors
+            counts_from_cluster, colors_centers = self.get_colors_cluster(image_no_bkg, number_of_colors=number_of_colors)  
+            
+            # find the average of close colors according to hue value
+            merged_centers, merged_labels = merge_clusters(colors_centers, quantile= None) 
+            counts_from_cluster = dict(counts_from_cluster.most_common())
+                        
+            numpixels =  np.zeros(len(counts_from_cluster), dtype = 'float'); updated_labels = []
+            colors_= np.zeros(merged_centers.shape, dtype = 'uint8')            
+            
+            for i, key in enumerate(counts_from_cluster.keys()): 
+                numpixels[i] =  counts_from_cluster[key]/num_pixels_in_mask 
+                colors_ [i] =  merged_centers[key]
+                updated_labels.append(merged_labels[key])
+                
+            pixsum = []; colors2 = []
+            for i in np.unique(merged_labels):
+                jj = updated_labels==i
+                pixsum.append( sum(numpixels[jj]) )
+                idxTrue = np.where(jj)[0][0]
+                colors2.append( colors_[idxTrue])
+                
+            sorted_indxs = np.flip( np.argsort(pixsum) )
+            if len(sorted_indxs)>max_num_colors: 
+                sorted_indxs=sorted_indxs[:max_num_colors]                
+            
+            numpixels_and_colors = []   
+            for i in sorted_indxs:
+                numpixels_and_colors.append([pixsum[i], colors2[i]])
+                            
+            self.top_k_colors.append(dict(numpixels_and_colors))
+    
+    def find_colors_via_clustering_3D_1D_cntPix(self, masks, masked_img, number_of_colors, max_num_colors, use_quantize=False):
+        ''' Clustering_3D_1D is based on three-values; R, G, B, and then, on 1D via h value from hsv
+        use_num_pixels_percentage (p) the percentage denotes the probability of a color, 
+        since we are picking max_num_colors out of the available 
+        number_of_colors the sum will not be 1, the sum will one
+        if and only if max_num_colors equals number_of_colors 
+        '''
+        use_num_pixels_percentage = True 
+        for label_id in range(len(self.labels)):   
+            num_pixels_in_mask = np.sum(masks[label_id]) if use_num_pixels_percentage else 1
+            image  = masked_img[label_id]           
+            image_no_bkg = self.remove_image_background(image, mask = masks[label_id])
+            
+            # use clustering to reduce the number of colors
+            counts_from_cluster, colors_centers = self.get_colors_cluster(image_no_bkg, number_of_colors=number_of_colors)  
+            
+            # finding the percentage of the color, something like the probability
+            for i, key in enumerate(counts_from_cluster.keys()): 
+                counts_from_cluster[i] =  counts_from_cluster[i]/num_pixels_in_mask 
+            
+            # find the average of similar colors according to hue value
+            grouped_centers, grouped_labels = merge_clusters(colors_centers, counts_from_cluster, quantile= None) 
+            counts_from_cluster = dict(counts_from_cluster.most_common())
+                        
+            
+            numpixels =  np.zeros(len(counts_from_cluster), dtype = 'float'); updated_labels = []
+            colors_= np.zeros(grouped_centers.shape, dtype = 'uint8')      
+            for i, key in enumerate(counts_from_cluster.keys()): 
+                numpixels[i] =  counts_from_cluster[key] # /num_pixels_in_mask 
+                colors_ [i] =  grouped_centers[key] # here we use grouped_centers instead of counts_from_cluster as we are interested in the new colors found according to the hue value
+                updated_labels.append(grouped_labels[key]) # we will be neededing the labels later
+            
+            # grouping colors according to similar labels find via the hue value
+            pixsum = []; colors_grouped = []
+            for i in np.unique(grouped_labels):
+                jj = updated_labels==i
+                pixsum.append( sum(numpixels[jj]) )
+                idxTrue = np.where(jj)[0][0]
+                colors_grouped.append( colors_[idxTrue])
+                
+            # sorting the indices according to pixel count    
+            sorted_indxs = np.flip(np.argsort(pixsum)) 
+            if len(sorted_indxs)>max_num_colors: 
+                sorted_indxs = sorted_indxs[:max_num_colors]                
+            #  pixels sums and corresponding colors
+            numpixels_and_colors = []   
+            for i in sorted_indxs:
+                numpixels_and_colors.append([pixsum[i], colors_grouped[i]])
+                            
+            self.top_k_colors.append(dict(numpixels_and_colors))
+
         
-         
+    def find_colors_via_quantize(self, masked_img, number_of_colors, max_num_colors, image):
+        for j in range(len(self.labels)):            
+            image = Image.fromarray(masked_img[j])
+            x_img = image.quantize(colors=number_of_colors, method=None, kmeans=0, palette=None)
+            z = x_img.convert('RGB').getcolors()
+            z.sort()
+            top_k_colors= z[-max_num_colors-1:-max_num_colors+1] 
+            self.top_k_colors.append(dict(top_k_colors))
+                
+        
     def __getitem__(self, ind):
-        return self.counts_top_k[ind],self.colors_centers[ind], \
-                    self.labels[ind], self.masked_img[ind]
+        item={}
+        for j in range(len(self.labels)):               
+            item[ self.labels[j] ] = self.top_k_colors[j]
+        
+        return item
     
 
     def __len__(self): 
-        return len(self.counts_top_k)
+        return len(self.max_num_colors)
+
 

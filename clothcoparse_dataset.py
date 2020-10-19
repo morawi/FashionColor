17575387b564ed2f6da98f40cbb3799517a473be
Mohammed Al-Rawi <36924479+morawi@users.noreply.github.com>
2020-10-19 13:18:43 +0100

    Added color bounds to clothing items

--- clothcoparse_dataset.py
+++ clothcoparse_dataset.py
@@ -9,16 +9,76 @@ import numpy as np
 
 
 def get_clothCoParse_class_names(): 
-    # names ordered according to label id, 0 for background and 59 for wedges
+    # Item names ordered according to label id, 0 for background and 59 for wedges
+    # A dictionary that contains each label name, and the possible (max) number of colors
+    # The number of colors can be changed, these are used as upper bounds when estimating 
+    # the number of colors in the item of the label
+       
+    min_ = 3  # min_num_colors
+    low_ = 5  # low_num_colors
+    mid_ = 8 # mid_num_colors
+    max_ = 17  # max_num_colors
     
-    class_names = ['background',  'accessories',  'bag',  'belt',  'blazer',
- 'blouse',  'bodysuit',  'boots',  'bra',  'bracelet',  'cape',  'cardigan',
- 'clogs', 'coat',  'dress', 'earrings', 'flats', 'glasses', 'gloves', 'hair',
- 'hat', 'heels', 'hoodie', 'intimate', 'jacket', 'jeans', 'jumper', 'leggings',
- 'loafers', 'necklace', 'panties', 'pants', 'pumps', 'purse', 'ring', 'romper',
- 'sandals', 'scarf', 'shirt', 'shoes', 'shorts', 'skin', 'skirt', 'sneakers',
- 'socks', 'stockings', 'suit', 'sunglasses', 'sweater', 'sweatshirt', 'swimwear',
- 't-shirt', 'tie', 'tights', 'top', 'vest', 'wallet', 'watch', 'wedges']
+    
+    class_names = {'background': 0,  # this will be ignored
+                   'accessories': low_,  
+                   'bag': mid_,  
+                   'belt': min_,  
+                   'blazer': max_,
+                   'blouse': max_,  
+                   'bodysuit': max_,  
+                   'boots': mid_,  
+                   'bra': mid_,  
+                   'bracelet': low_,  
+                   'cape': max_,  
+                   'cardigan': max_,
+                   'clogs': mid_, 
+                   'coat': max_,  
+                   'dress': max_, 
+                   'earrings': min_, 
+                   'flats': low_, 
+                   'glasses': min_, 
+                   'gloves': low_, 
+                   'hair': mid_, # I had to add this up, because sometimes women's hair has a lot of colors 
+                   'hat': mid_, 
+                   'heels': min_, 
+                   'hoodie': max_, 
+                   'intimate': max_, 
+                   'jacket': max_, 
+                   'jeans': max_, 
+                   'jumper': max_, 
+                   'leggings': max_,
+                   'loafers': low_, 
+                   'necklace': min_, 
+                   'panties':min_, 
+                   'pants': max_, 
+                   'pumps': low_, # there are multicolor pumps, but since this item will appear small in the image, it will be hard to get the colors
+                   'purse': mid_, 
+                   'ring': min_, 
+                   'romper': max_,
+                   'sandals': low_, 
+                   'scarf': max_, 
+                   'shirt': max_, 
+                   'shoes': low_, 
+                   'shorts': max_, 
+                   'skin': min_,   # skin has one color
+                   'skirt': max_, 
+                   'sneakers': mid_,
+                   'socks': low_, 
+                   'stockings': mid_, 
+                   'suit': max_, 
+                   'sunglasses': min_, 
+                   'sweater': max_,
+                   'sweatshirt':max_, 
+                   'swimwear': max_,
+                   't-shirt': max_,
+                   'tie': max_,
+                   'tights': max_, 
+                   'top': max_,
+                   'vest': max_,
+                   'wallet': min_, 
+                   'watch': min_,
+                   'wedges': mid_}
     
     return class_names
         
@@ -26,7 +86,7 @@ def get_clothCoParse_class_names():
 class ImageDataset(Dataset):
     def __init__(self, root, mode="train",  HPC_run=False):
         
-        self.class_names = get_clothCoParse_class_names()
+        self.class_names = list(get_clothCoParse_class_names().keys())
         
         if HPC_run:
             root = '/home/malrawi/MyPrograms/Data/ClothCoParse'
@@ -35,7 +95,7 @@ class ImageDataset(Dataset):
         self.files_B = sorted(glob.glob(os.path.join(root, "%s/B" % mode) + "/*.*")) # get the target image file-names
         
     def number_of_classes(self, opt):
-        return(len(get_clothCoParse_class_names())) # this should do
+        return(len(self.class_names)) # this should do
   
 
     def __getitem__(self, index):   
@@ -57,8 +117,7 @@ class ImageDataset(Dataset):
             labels.append(self.class_names[obj_ids[i]])
                               
         image_id = index
-        fname = os.path.basename(self.files_A[index % len(self.files_A)])
-          
+        fname = os.path.basename(self.files_A[index % len(self.files_A)])          
         
         return image_A, masked_img, labels, image_id, masks, fname
      
